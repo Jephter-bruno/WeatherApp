7d91f8b6a4b4786df676384a5720477d60a2183f
Jephter-bruno <jephter35679577@gmail.com>
2023-04-19 22:40:46 +0300

    Added RetrofitClient and WeatherApiSearvice

--- app/src/main/java/com/jephter/weatherapp/HomeActivity.kt
+++ app/src/main/java/com/jephter/weatherapp/HomeActivity.kt
@@ -2,10 +2,28 @@ package com.jephter.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ListView
 
 class HomeActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val listView = findViewById<ListView>(R.id.listView)
+        val searchbutton = findViewById<ImageView>(R.id.searchbutton)
+
+
+        searchbutton.setOnClickListener{
+            val searchView = findViewById<EditText>(R.id.searchView)
+            searchView.visibility=  View.GONE
+        }
+
+
+
     }
 }
